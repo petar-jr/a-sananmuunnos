a610e67149a791902385bed7cd1e1d213fb2eddc
Petar Teodosin Jr <petar@phz.fi>
2020-09-29 17:47:47 +0300

    Initial. Dummy service

---
+++ src/main/java/org/isokissa/sananmuunnosapi/SananmuunnosApiApplication.java
@@ -0,0 +1,13 @@
+package org.isokissa.sananmuunnosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SananmuunnosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SananmuunnosApiApplication.class, args);
+	}
+
+}
