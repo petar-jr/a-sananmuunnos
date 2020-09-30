55066c11bc9dd70c30297429357e2c0748c84c38
Petar Teodosin Jr <petar@phz.fi>
2020-09-30 17:00:44 +0300

    Add tests and implementation

--- README.md
+++ README.md
@@ -7,13 +7,14 @@
 ## Run service in testing environment
 
     ./gradlew :bootRun
-
-By default it is con
+    
+It is configured in `application.properties` to listen to port 8088. Access the service
+with url `http://localhost:8088/sananmuunnos`
 
 ## Create docker image
 
 For convenient deployment, Spring's Gradle plugin is able to build a docker image, with
-name `isokissa/sannanmuunnos-api`:
+name `isokissa/sananmuunnos-api`:
 
     ./gradlew bootBuildImage
 
@@ -25,20 +26,4 @@ To run the image:
 
 For example, if the service is running on localhost:8088:
 
-    curl -X POST -d "CHANGE ME" http://localhost:8088/sananmuunnos
-
-
-### Reference Documentation
-
-For further reference, please consider the following sections:
-
-- [Official Gradle documentation](https://docs.gradle.org)
-- [Spring Boot Gradle Plugin Reference Guide](https://docs.spring.io/spring-boot/docs/2.3.4.RELEASE/gradle-plugin/reference/html/)
-- [Create an OCI image](https://docs.spring.io/spring-boot/docs/2.3.4.RELEASE/gradle-plugin/reference/html/#build-image)
-- [Spring Boot DevTools](https://docs.spring.io/spring-boot/docs/2.3.4.RELEASE/reference/htmlsingle/#using-boot-devtools)
-
-### Additional Links
-
-These additional references should also help you:
-
-- [Gradle Build Scans – insights for your project's build](https://scans.gradle.com#gradle)
+    curl -H "Content-Type: application/json" -X POST -d 'outget "input"' http://localhost:8088/sananmuunnos
---
+++ src/test/java/org/isokissa/sananmuunnos/SplitterTest.java
@@ -0,0 +1,57 @@
+package org.isokissa.sananmuunnos;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class SplitterTest {
+    
+    @Test
+    public void splitsEmptyString() {
+        List<String> result = Splitter.splitToTokens("");
+        assertThat(result.size()).isEqualTo(0);
+    }
+
+    @Test
+    public void splitsStringWithOneWord() {
+        List<String> result = Splitter.splitToTokens("abc");
+        assertThat(result.size()).isEqualTo(1);
+        assertThat(result.get(0)).isEqualTo("abc");
+    }
+
+    @Test
+    public void splitsStringWithTwoWords() {
+        List<String> result = Splitter.splitToTokens("abc  ");
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result.get(0)).isEqualTo("abc");
+        assertThat(result.get(1)).isEqualTo("  ");
+    }
+
+    @Test
+    public void splitsStringWithTwoWordsStartingWithSpaces() {
+        List<String> result = Splitter.splitToTokens("    abc  ");
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get(0)).isEqualTo("    ");
+        assertThat(result.get(1)).isEqualTo("abc");
+        assertThat(result.get(2)).isEqualTo("  ");
+    }
+
+    @Test
+    public void findsTheBeginningAndRest() {
+        checkTheBeginningAndRest("", "", "");
+        checkTheBeginningAndRest("b", "b", "");
+        checkTheBeginningAndRest("ba", "ba", "");
+        checkTheBeginningAndRest("bac", "ba", "c");
+        checkTheBeginningAndRest("baa", "baa", "");
+        checkTheBeginningAndRest("baaca", "baa", "ca");
+        checkTheBeginningAndRest("!baaca", "!", "baaca");
+        checkTheBeginningAndRest("ba'aca", "ba", "'aca");
+    }
+
+    private void checkTheBeginningAndRest(String word, String expectedBeginning, String expectedRest) {
+        Splitter.WordParts wordParts = Splitter.splitWord(word);
+        assertThat(wordParts.getBeginning()).isEqualTo(expectedBeginning);
+        assertThat(wordParts.getRest()).isEqualTo(expectedRest);
+    }
+}
